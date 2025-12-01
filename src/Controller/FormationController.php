3000067fa987affe127757e59264342df8e0dd89
Suzanne Pons <suzanne.pons68@gmail.com>
2025-12-01 17:02:13 +0100

    Création Controller formation et home

---
+++ src/Controller/FormationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FormationRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class FormationController extends AbstractController
+{
+    #[Route('/formation', name: 'app_formation')]
+    public function index(FormationRepository $formationRepository): Response
+    {
+
+        $formations = $formationRepository->findBy([], ['title' => 'ASC']);
+
+        return $this->render('formation/index.html.twig', [
+            'formations' => 'formations',
+        ]);
+    }
+}
